396b5bcb3be07f2eee154fc6d571bcb72d285def
Alliyah Myka Bibat <mykabibat@gmail.com>
2026-04-20 20:24:42 +0800

    fixed fonts and pathing

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
       <div className="hero">
         <img src="/logo.png" alt="logo" className="logo" />
         <h1 style={{ fontFamily: "AliensAndCows" }}>
-          Movie App~ 🎬
+          Movies~~ 🎬
         </h1>
       </div>
 
